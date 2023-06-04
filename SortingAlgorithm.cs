36224f857034b802d1212f5ccf0977e6ab9db4ca
Titoriano Broadus <titoriano@gmail.com>
2023-06-04 12:36:00 -0500

    clean up event, still have reset errors

--- SortingAlgorithm.cs
+++ SortingAlgorithm.cs
@@ -6,40 +6,20 @@ using SAG = SortingAlgorithmDictionary;
 
 public partial class SortingAlgorithm : Node2D
 {
-    [Export]
-    public SpinBox CountInput { get; set; }
+    [Export] public SpinBox CountInput { get; set; }
+    [Export] public Button ResetButton { get; set; }
+    [Export] public bool UseViewPortSize { get; set; } = true;
+    [Export] public Vector2 Size { get; set; } = new Vector2 { X = 300, Y = 300 };
+    [Export] public Label StatLabel { get; set; }
+    [Export] public bool CompareSwaps { get; set; }
+    [Export] public double DelayInSeconds { get; set; } = .25;
+    [Export] public Button SpeedDownButton { get; set; }
+    [Export] public Button SpeedUpButton { get; set; }
+    [Export] public OptionButton AlgorithmList { get; set; }
+    [Export] public OptionButton SwapsOrComparisons { get; set; }
 
-    [Export]
-    public Button ResetButton { get; set; }
-
-    [Export]
-    public bool UseViewPortSize { get; set; } = true;
-
-    [Export]
-    public Vector2 Size { get; set; } = new Vector2 { X = 300, Y = 300 };
-
-    [Export]
-    public Label StatLabel { get; set; }
-
-    [Export]
-    public bool CompareSwaps { get; set; }
-
-    [Export]
-    public double DelayInSeconds { get; set; } = .25;
-
-    [Export]
-    public Button SpeedDownButton { get; set; }
-
-    [Export]
-    public Button SpeedUpButton { get; set; }
-
-    [Export]
-    public OptionButton AlgorithmList { get; set; }
-
-    [Export]
-    public OptionButton SwapsOrComparisons { get; set; }
-
-    private int _count { get => (int)CountInput.Value; }
+    private ISortingAlgorithm _algorithm;
+    private int N { get => (int)CountInput.Value; }
     private float _width;
     private float _height;
     private int[] _data = new int[0];
@@ -64,22 +44,19 @@ public partial class SortingAlgorithm : Node2D
 
     private void InitializeHandlers()
     {
+        double delayInc = .05;
         SpeedDownButton.Pressed += () =>
         {
-            if (DelayInSeconds <= 0) SpeedUpButton.Disabled = false;
-
-            DelayInSeconds = DelayInSeconds + .02 > 1 ? 1 : DelayInSeconds + .02;
-
+            if (DelayInSeconds <= delayInc) SpeedUpButton.Disabled = false;
+            DelayInSeconds = DelayInSeconds + delayInc > 1 ? 1 : DelayInSeconds + delayInc;
             if (DelayInSeconds >= 1) SpeedDownButton.Disabled = true;
         };
 
         SpeedUpButton.Pressed += () =>
         {
             if (DelayInSeconds >= 1) SpeedDownButton.Disabled = false;
-
-            DelayInSeconds = DelayInSeconds - .02 < .02 ? .02 : DelayInSeconds - .02;
-
-            if (DelayInSeconds <= 0) SpeedUpButton.Disabled = true;
+            DelayInSeconds = DelayInSeconds - delayInc < delayInc ? delayInc : DelayInSeconds - .02;
+            if (DelayInSeconds <= delayInc) SpeedUpButton.Disabled = true;
         };
 
         SAG.Instance.Keys.ToList().ForEach(i => AlgorithmList.AddItem(i.ToString()));
@@ -113,36 +90,35 @@ public partial class SortingAlgorithm : Node2D
         };
     }
 
-    private void UpdateStats() => StatLabel.Text = $"{AlgorithmList.Text} Swaps:{_swapCount,2} Comparisons:{_compareCount,2}";
+    private void UpdatePointers(int i, int j)
+    {
+        (_pointer1, _pointer2) = (i, j);
+        StatLabel.Text = $"{AlgorithmList.Text} Swaps:{_swapCount,2} Comparisons:{_compareCount,2}";
+        QueueRedraw();
+    }
 
     private void HandleSwap(int i, int j)
     {
         Task.Delay((int)(DelayInSeconds * 1000)).Wait();
-        _swapCount++;
-        _pointer1 = i;
-        _pointer2 = j;
         _data.Swap(i, j);
-        QueueRedraw();
-        UpdateStats();
+        _swapCount++;
+        UpdatePointers(i, j);
     }
 
     private void HandleComparison(int i, int j)
     {
         Task.Delay((int)(DelayInSeconds * 1000)).Wait();
         _compareCount++;
-        _pointer1 = i;
-        _pointer2 = j;
-        QueueRedraw();
-        UpdateStats();
+        UpdatePointers(i, j);
     }
 
     private void InitializeDataAndShuffle()
     {
-        _data = Enumerable.Range(0, _count).ToArray();
+        _data = Enumerable.Range(0, N).ToArray();
         var random = new Random();
-        for (int i = 0; i < _count; i++)
+        for (int i = 0; i < N; i++)
         {
-            var j = random.Next(0, _count);
+            var j = random.Next(0, N);
             var temp = _data[i];
             _data[i] = _data[j];
             _data[j] = temp;
@@ -151,8 +127,13 @@ public partial class SortingAlgorithm : Node2D
 
     private void Reset(SortingAlgorithmType algorithmType)
     {
+        if (_algorithm != null)
+        {
+            _algorithm.Swapped -= HandleSwap;
+            _algorithm.Compared -= HandleComparison;
+        }
+
         InitializeDataAndShuffle();
-        QueueRedraw();
 
         _pointer1 = 0;
         _pointer2 = 0;
@@ -161,19 +142,19 @@ public partial class SortingAlgorithm : Node2D
 
         if (algorithmType != SortingAlgorithmType.None)
         {
-            var alg = SAG.Instance[algorithmType];
+            _algorithm = SAG.Instance[algorithmType];
 
-            alg.Swapped += (int i, int j) => HandleSwap(i, j);
+            _algorithm.Swapped += HandleSwap;
+            if (!CompareSwaps) _algorithm.Compared += HandleComparison;
 
-            if (!CompareSwaps) alg.Compared += (int i, int j) => HandleComparison(i, j);
-
-            Task.Run(() => alg.Sort(_data.Clone() as int[], _data.Length));
-            UpdateStats();
+            Task.Run(() => _algorithm.Sort(_data.Clone() as int[], _data.Length)).ConfigureAwait(false);
         }
         else
         {
             StatLabel.Text = "Select an algorithm";
         }
+
+        QueueRedraw();
     }
 
     public override void _Process(double delta) { }
@@ -186,7 +167,7 @@ public partial class SortingAlgorithm : Node2D
 
         for (int i = 0; i < _data.Length; i++)
         {
-            var height = (_data[i] + 1) / ((float)_count + 1) * _height;
+            var height = (_data[i] + 1) / ((float)N + 1) * _height;
             var x = i * (width + spacer);
             var y = _height - height;
 
